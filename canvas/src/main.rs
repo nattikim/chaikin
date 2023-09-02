b18d42626535900cf93f6feec9c15774ae93cae8
Maksim Mikhailov <max@mer.pw>
2023-09-02 19:48:15 +0300

    Use chaikin in canvas

--- canvas/src/main.rs
+++ canvas/src/main.rs
@@ -1,8 +1,11 @@
+use chaikin::chaikin;
 use macroquad::prelude::*;
 
 #[macroquad::main("Chaikin in Rust", window_conf)]
 async fn main() {
     let mut control_points: Vec<(f32, f32)> = Vec::new();
+
+    let mut points: Vec<(f32, f32)> = vec![];
 
     loop {
         clear_background(DARKGRAY);
@@ -12,12 +15,20 @@ async fn main() {
             control_points.push(mouse_position);
         }
 
-        for point in &control_points {
-            draw_circle(point.0, point.1, 3.0, GREEN);
+        if is_key_pressed(KeyCode::Enter) && control_points.len() > 1 {
+            points = chaikin(&control_points);
         }
 
         if is_key_pressed(KeyCode::Escape) {
             break;
+        }
+
+        for point in &points {
+            draw_circle(point.0, point.1, 3.0, BLUE);
+        }
+
+        for point in &control_points {
+            draw_circle(point.0, point.1, 4.0, GREEN);
         }
 
         next_frame().await;
