5bc8ed1fe1d638e3eb0d38cef35b851afde77900
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-09-02 19:21:41 +0300

    add macroquad lib
    init the screen with points

--- canvas/src/main.rs
+++ canvas/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Chaikin in Rust", window_conf)]
+async fn main() {
+    let mut control_points: Vec<(f32, f32)> = Vec::new();
+
+    loop {
+        clear_background(DARKGRAY);
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let mouse_position = mouse_position();
+            control_points.push(mouse_position);
+        }
+
+        for point in &control_points {
+            draw_circle(point.0, point.1, 3.0, GREEN);
+        }
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
+
+        next_frame().await;
+    }
 }
