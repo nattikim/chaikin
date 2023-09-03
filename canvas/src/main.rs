b20c62f3c927d7f057f68f2a227a492bd8a8d5de
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-09-03 17:05:40 +0300

    implement basic features to draw lines using chaikin algorithm
    plus bonus feature to restart

--- canvas/src/main.rs
+++ canvas/src/main.rs
@@ -1,35 +1,77 @@
+use canvas::*;
 use chaikin::chaikin;
+use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 
 #[macroquad::main("Chaikin in Rust", window_conf)]
 async fn main() {
-    let mut control_points: Vec<(f32, f32)> = Vec::new();
+    set_window_size(800, 800);
 
+    let mut control_points: Vec<(f32, f32)> = Vec::new();
+    let mut init_points: Vec<(f32, f32)> = Vec::new();
     let mut points: Vec<(f32, f32)> = vec![];
+    let mut lines: Vec<(f32, f32)> = Vec::new();
+
+    let mut animation_step = 0;
+    let mut animation_timer = 0.0;
+    let animation_speed = 1.0;
+    let mut start = false;
 
     loop {
         clear_background(DARKGRAY);
 
-        if is_mouse_button_pressed(MouseButton::Left) {
+        draw_instructions();
+
+        if is_mouse_button_pressed(MouseButton::Left) && !start {
             let mouse_position = mouse_position();
             control_points.push(mouse_position);
         }
 
-        if is_key_pressed(KeyCode::Enter) && control_points.len() > 1 {
-            points = chaikin(&control_points);
+        if is_key_pressed(KeyCode::Enter) {
+            start = true;
+            init_points = control_points.clone();
+        }
+
+        animation_timer += get_frame_time();
+
+        if start && init_points.len() > 1 && animation_step < 7 && animation_timer > animation_speed
+        {
+            animation_timer = 0.0;
+
+            points = chaikin(&init_points);
+
+            init_points = points.clone();
+            animation_step += 1;
+
+            lines.clear();
+            for i in 0..points.len() - 1 {
+                lines.push(points[i]);
+                lines.push(points[i + 1]);
+            }
+
+            draw_points(&control_points)
+        }
+        draw_lines(&lines);
+
+        if is_key_pressed(KeyCode::R) {
+            animation_step = 0;
+            animation_timer = 0.0;
+            control_points.clear();
+            init_points.clear();
+            points.clear();
+            lines.clear();
+            start = false;
         }
 
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
 
-        for point in &points {
-            draw_circle(point.0, point.1, 3.0, BLUE);
-        }
+        //if !start {
+        draw_points(&control_points);
+        //}
 
-        for point in &control_points {
-            draw_circle(point.0, point.1, 4.0, GREEN);
-        }
+        draw_counter(animation_step);
 
         next_frame().await;
     }
