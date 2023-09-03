b6b5fabad7fa03602d7a78c7f410bc1eb6aff055
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-09-03 17:31:57 +0300

    refactor code
    group all vars to a struct
    break features to a functions (for user input, animation, reset and drawings

--- canvas/src/lib.rs
+++ canvas/src/lib.rs
@@ -1,4 +1,75 @@
+use chaikin::chaikin;
 use macroquad::prelude::*;
+
+pub struct AppState {
+    pub control_points: Vec<(f32, f32)>,
+    init_points: Vec<(f32, f32)>,
+    points: Vec<(f32, f32)>,
+    pub lines: Vec<(f32, f32)>,
+    pub animation_step: i32,
+    animation_timer: f32,
+    animation_speed: f32,
+    start: bool,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+            control_points: Vec::new(),
+            init_points: Vec::new(),
+            points: Vec::new(),
+            lines: Vec::new(),
+            animation_step: 0,
+            animation_timer: 0.0,
+            animation_speed: 1.0,
+            start: false,
+        }
+    }
+}
+pub fn handle_input(state: &mut AppState) {
+    if is_mouse_button_pressed(MouseButton::Left) && !state.start {
+        let mouse_position = mouse_position();
+        state.control_points.push(mouse_position);
+    }
+
+    if is_key_pressed(KeyCode::Enter) {
+        state.start = true;
+        state.init_points = state.control_points.clone();
+    }
+}
+
+pub fn animation_step(state: &mut AppState) {
+    state.animation_timer += get_frame_time();
+
+    if state.start
+        && state.init_points.len() > 1
+        && state.animation_step < 7
+        && state.animation_timer > state.animation_speed
+    {
+        state.animation_timer = 0.0;
+
+        state.points = chaikin(&state.init_points);
+
+        state.init_points = state.points.clone();
+        state.animation_step += 1;
+
+        state.lines.clear();
+        for i in 0..state.points.len() - 1 {
+            state.lines.push(state.points[i]);
+            state.lines.push(state.points[i + 1]);
+        }
+    }
+}
+
+pub fn reset_state(state: &mut AppState) {
+    state.animation_step = 0;
+    state.animation_timer = 0.0;
+    state.control_points.clear();
+    state.init_points.clear();
+    state.points.clear();
+    state.lines.clear();
+    state.start = false;
+}
 
 pub fn draw_lines(lines: &[(f32, f32)]) {
     for i in 0..lines.len() / 2 {
@@ -22,8 +93,8 @@ pub fn draw_points(control_points: &[(f32, f32)]) {
 pub fn draw_instructions() {
     draw_text(
         "Place points and press Enter. Press R to restart.",
-        15.0,
-        20.0,
+        177.0,
+        30.0,
         20.0,
         GRAY,
     );
--- canvas/src/main.rs
+++ canvas/src/main.rs
@@ -1,77 +1,33 @@
 use canvas::*;
-use chaikin::chaikin;
 use macroquad::miniquad::window::set_window_size;
 use macroquad::prelude::*;
 
 #[macroquad::main("Chaikin in Rust", window_conf)]
 async fn main() {
+    let mut state = AppState::default();
+
     set_window_size(800, 800);
-
-    let mut control_points: Vec<(f32, f32)> = Vec::new();
-    let mut init_points: Vec<(f32, f32)> = Vec::new();
-    let mut points: Vec<(f32, f32)> = vec![];
-    let mut lines: Vec<(f32, f32)> = Vec::new();
-
-    let mut animation_step = 0;
-    let mut animation_timer = 0.0;
-    let animation_speed = 1.0;
-    let mut start = false;
 
     loop {
         clear_background(DARKGRAY);
 
         draw_instructions();
 
-        if is_mouse_button_pressed(MouseButton::Left) && !start {
-            let mouse_position = mouse_position();
-            control_points.push(mouse_position);
-        }
-
-        if is_key_pressed(KeyCode::Enter) {
-            start = true;
-            init_points = control_points.clone();
-        }
-
-        animation_timer += get_frame_time();
-
-        if start && init_points.len() > 1 && animation_step < 7 && animation_timer > animation_speed
-        {
-            animation_timer = 0.0;
-
-            points = chaikin(&init_points);
-
-            init_points = points.clone();
-            animation_step += 1;
-
-            lines.clear();
-            for i in 0..points.len() - 1 {
-                lines.push(points[i]);
-                lines.push(points[i + 1]);
-            }
-
-            draw_points(&control_points)
-        }
-        draw_lines(&lines);
+        handle_input(&mut state);
+        animation_step(&mut state);
 
         if is_key_pressed(KeyCode::R) {
-            animation_step = 0;
-            animation_timer = 0.0;
-            control_points.clear();
-            init_points.clear();
-            points.clear();
-            lines.clear();
-            start = false;
+            reset_state(&mut state);
         }
 
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
 
-        //if !start {
-        draw_points(&control_points);
-        //}
+        draw_points(&state.control_points);
+        draw_lines(&state.lines);
 
-        draw_counter(animation_step);
+        draw_counter(state.animation_step);
 
         next_frame().await;
     }
